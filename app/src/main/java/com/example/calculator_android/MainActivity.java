377675eddc11033f4618eba3d37511a2f9e979d8
Rafael Vieira <rafavs09@gmail.com>
2025-01-17 11:50:58 -0300

    Almost completed calculator Changed hardcode problem created a style for buttons and created new colors Altered how the calculator works, now it does not use another dependencie for calculate

--- app/src/main/java/com/example/calculator_android/MainActivity.java
+++ app/src/main/java/com/example/calculator_android/MainActivity.java
@@ -1,120 +1,132 @@
 package com.example.calculator_android;
-
 import android.os.Bundle;
-import android.view.ActionProvider;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.material.button.MaterialButton;
-
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
-
-
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    TextView screenResult, screenCalculation;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9,buttonCe,buttonBackspace,
-    buttonOpenParentheses, buttonCloseParentheses,buttonDivide, buttonMultiply, buttonMinus, buttonSum, buttonEquals, buttonDot;
+    double first_num;
+    String operation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        screenResult = findViewById(R.id.results);
-        screenCalculation = findViewById(R.id.calculation);
+        TextView screenResult = findViewById(R.id.results);
 
-        assingId(button0, R.id.zero);
-        assingId(button1, R.id.one);
-        assingId(button2, R.id.two);
-        assingId(button3, R.id.three);
-        assingId(button4, R.id.four);
-        assingId(button5, R.id.five);
-        assingId(button6, R.id.six);
-        assingId(button7, R.id.seven);
-        assingId(button8, R.id.eight);
-        assingId(button9, R.id.nine);
-        assingId(buttonCe, R.id.ce);
-        assingId(buttonBackspace, R.id.backspace);
-        assingId(buttonOpenParentheses, R.id.parenthesesOne);
-        assingId(buttonCloseParentheses, R.id.parenthesesTwo);
-        assingId(buttonDivide, R.id.division);
-        assingId(buttonMultiply, R.id.multiplication);
-        assingId(buttonMinus, R.id.decrement);
-        assingId(buttonSum, R.id.sum);
-        assingId(buttonEquals, R.id.equals);
-        assingId(buttonDot, R.id.dot);
-
-    }
-
-    void assingId(MaterialButton btn, int id) {
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
-    }
-
-    void Cal(TextView screenCalculation) {
-
-    }
+        Button num0 = findViewById(R.id.zero);
+        Button num1 = findViewById(R.id.one);
+        Button num2 = findViewById(R.id.two);
+        Button num3 = findViewById(R.id.three);
+        Button num4 = findViewById(R.id.four);
+        Button num5 = findViewById(R.id.five);
+        Button num6 = findViewById(R.id.six);
+        Button num7 = findViewById(R.id.seven);
+        Button num8 = findViewById(R.id.eight);
+        Button num9 = findViewById(R.id.nine);
+        Button sum = findViewById(R.id.sum_button);
+        Button minus = findViewById(R.id.minus_button);
+        Button multiplication = findViewById(R.id.multiplication_button);
+        Button division = findViewById(R.id.division_button);
+        Button dot = findViewById(R.id.dot_button);
+        Button equals = findViewById(R.id.equals_button);
+        Button ac_button = findViewById(R.id.ac_button);
+        Button backspace = findViewById(R.id.backspace);
+        Button backspace_2 = findViewById(R.id.backspace_2);
 
 
-    @Override
-    public void onClick(View v) {
-        MaterialButton button = (MaterialButton) v;
-        String buttonText = button.getText().toString();
-        String calculationData = screenCalculation.getText().toString();
-
-        if(buttonText.equals("CE")) {
-            screenCalculation.setText("");
+        ac_button.setOnClickListener(View -> {
+            first_num = 0;
             screenResult.setText("0");
-            return;
+        });
+
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(num0);
+        nums.add(num1);
+        nums.add(num2);
+        nums.add(num3);
+        nums.add(num4);
+        nums.add(num5);
+        nums.add(num6);
+        nums.add(num7);
+        nums.add(num8);
+        nums.add(num9);
+
+        for (Button b : nums) {
+            b.setOnClickListener(view -> {
+                if (!screenResult.getText().toString().equals("0")) {
+                    screenResult.setText(screenResult.getText().toString() + b.getText().toString());
+                } else {
+                    screenResult.setText(b.getText().toString());
+                }
+
+            });
         }
-        if(buttonText.equals("=")) {
-            screenResult.setText(screenCalculation.getText());
-            screenCalculation.setText("");
-            return;
+        ArrayList<Button> operators = new ArrayList<>();
+        operators.add(sum);
+        operators.add(minus);
+        operators.add(multiplication);
+        operators.add(division);
+
+
+        for (Button b: operators) {
+            b.setOnClickListener(view -> {
+                first_num = Double.parseDouble(screenResult.getText().toString());
+                operation = b.getText().toString();
+                screenResult.setText("0");
+            });
         }
-        if(buttonText.equals("C")) {
-            if (calculationData.length() < ) {
-                calculationData = calculationData.substring(1, calculationData.length() - 1);
+
+        backspace.setOnClickListener((view -> {
+            String num = screenResult.getText().toString();
+            if (num.length()>1){
+                screenResult.setText(num.substring(0,num.length()-1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                screenResult.setText("0");
             }
-            else { screenCalculation.setText("0");
+        }));
 
+        backspace_2.setOnClickListener((view -> {
+            String num = screenResult.getText().toString();
+            if (num.length()>1){
+                screenResult.setText(num.substring(0,num.length()-1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                screenResult.setText("0");
+            }
+        }));
+
+        dot.setOnClickListener(view -> {
+            if (!screenResult.getText().toString().contains(".")) {
+                screenResult.setText(screenResult.getText().toString() + ".");
+            }
+        });
+
+        equals.setOnClickListener(view ->{
+            double second_num = Double.parseDouble(screenResult.getText().toString());
+            double result;
+            switch (operation) {
+                case "-":
+                    result = first_num-second_num;
+                    break;
+                case "+":
+                    result = first_num+second_num;
+                    break;
+                case "*":
+                    result = first_num*second_num;
+                    break;
+                case "/":
+                    result = first_num/second_num;
+                    break;
+
+                default:
+                    result = first_num+second_num;
             }
 
-        } else {
-                calculationData = calculationData + buttonText;
-
-        }
-        screenCalculation.setText(calculationData);
-        String finalResult = getResults(calculationData);
-
-        if (!finalResult.equals("Error")) {
-            screenResult.setText(finalResult);
-        }
-
+            screenResult.setText(String.valueOf(result));
+            first_num = result;
+        });
     }
-
-    String getResults(String data) {
-        try {
-            Context context = Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable = context.initStandardObjects();
-            String finalResult = context.evaluateString(scriptable,data, "Javascript", 1, null).toString();
-            return finalResult;
-
-        } catch (Exception e) {
-            return "Error";
-        }
-    }
-
 }
